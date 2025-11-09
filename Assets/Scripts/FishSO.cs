8e161c960a0c69ce8bd8d023afb2cb6878ce65f8
Ashley Setter <assetter@hotmail.com>
2025-11-09 23:37:18 +0000

    fix typo

--- Assets/Scripts/FishSO.cs
+++ Assets/Scripts/FishSO.cs
@@ -7,7 +7,7 @@ public class FishSO : ScriptableObject
     public float minFightTime; // min time spent fighting
     public float maxFightTime; // max time spent fighting
     public float reelDistance; // distance reeled by a reel action
-    public float fightOnReelChance; // change that it begins fighting again on a reel action
+    public float fightOnReelChance; // chance that it begins fighting again on a reel action
     public float fishWeight; // unit of weight for fish - used to determine how many cats it feeds and value when sold
     public Sprite fishSprite;
 }
